410af987e923f1a0225662411c8ce41fc312cade
daniDevCrisar <llamadas.ia.crisar@gmail.com>
2026-04-15 09:24:14 -0500

    reporte 14-04

--- bd_sql.sql
+++ bd_sql.sql
@@ -394,6 +394,7 @@ CREATE TABLE llamadas (
     ia_error_interpretacion TINYINT(1) DEFAULT 0,
     ia_dice_variable TINYINT(1) DEFAULT 0,
     ia_mala_pronunciacion TINYINT(1) DEFAULT 0,
+    ia_cuelga_sin_finalizar TINYINT(1) DEFAULT 0,
 
     conductor_cuelga TINYINT(1) DEFAULT 0,
     conductor_no_contesta TINYINT(1) DEFAULT 0,
@@ -502,6 +503,7 @@ CREATE TABLE tmp_lotes_det (
     ia_error_interpretacion VARCHAR(10),
     ia_dice_variable VARCHAR(10),
     ia_mala_pronunciacion VARCHAR(10),
+    ia_cuelga_sin_finalizar VARCHAR(10),
 
     conductor_cuelga VARCHAR(10),
     conductor_no_contesta VARCHAR(10),
