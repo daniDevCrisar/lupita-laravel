11799bc47eaf9fcdcc3768839661d21d1a97099b
daniDevCrisar <llamadas.ia.crisar@gmail.com>
2026-04-25 14:49:34 -0500

    se logro obtener lista de ref de historial ia

--- app/Http/Controllers/API/LlamadasApiController.php
+++ app/Http/Controllers/API/LlamadasApiController.php
@@ -11,7 +11,9 @@ class LlamadasApiController extends Controller
     public function devolver_lista_lote_refs($lote_id){
 
         //------------DATOS DE LLAMADAS CON REF------------
-        $sql="select ref from tmp_lotes_det where lote_id=?;";
+        $sql="
+        select DISTINCT(ref) from tmp_lotes_det where lote_id=? and ref !='' ;
+        ";
         $refs= DB::select($sql,[$lote_id]);
         foreach($refs as $ref)
             $lista[]=$ref->ref;
@@ -19,7 +21,7 @@ class LlamadasApiController extends Controller
         //-------------FECHAS DE LLAMADAS DE CONFIRMACION SIN REF-------------
         $sql_conf="
         select DISTINCT(a.fecha) from
-             (SELECT DATE_FORMAT(FROM_UNIXTIME(CAST(created_at AS UNSIGNED) / 1000) ,'%d/%m/%Y') as fecha
+             (SELECT DATE_FORMAT(FROM_UNIXTIME(CAST(created_at AS UNSIGNED) / 1000) ,'%Y-%m-%d') as fecha
               FROM `tmp_lotes_det` WHERE lote_id=?) as a;
         ";
         $fechas_conf= DB::select($sql_conf,[$lote_id]);
@@ -46,13 +48,23 @@ class LlamadasApiController extends Controller
         // ----------------------ACTUALIZAR TRTS DEL LOTE*-------------------------
         $sql="update tmp_lotes_det set transportista=? where lote_id=? and ref=? and transportista='';
         ";
+        //-------------------SQL PARA CONFIRMACIONES-------------
+        $sql_conf="
+        UPDATE `tmp_lotes_det` set ref=?, transportista=? where lote_id=? and llamada_tipo='1' and placa=? and
+        DATE_FORMAT(FROM_UNIXTIME(CAST(created_at AS UNSIGNED) / 1000) ,'%d/%m/%Y')=?;
+        ";
+
         foreach ($datos as $key => $item) {
             //normalizar texto------------------
             array_walk_recursive($item, function(&$i, $key) {
                 if (is_string($i)) $i = strtoupper( html_entity_decode($i, ENT_QUOTES, 'UTF-8'));
             });
             //------------------------------------------
-            DB::update($sql,[$item['transportista'],$lote_id,$key]);
+            if ($item['confirmacion']){
+                $fecha_llamada_buscar= explode(' ',$item['fecha_llamada']);
+                DB::update($sql_conf,[$key,$item['transportista'],$lote_id,$item['placa'],$fecha_llamada_buscar[0] ]);
+            }else
+                DB::update($sql,[$item['transportista'],$lote_id,$key]);
         }
 
         //-************************************************************************
